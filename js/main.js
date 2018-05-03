7d40ebb1e3e67037cdf2da2c2d78794771d52813
Tech-A64 <tech-a64@Tech-A64s-MacBook-Air.local>
2018-05-03 16:50:56 +0100

    Thursday 16.50

--- js/main.js
+++ js/main.js
@@ -4,9 +4,21 @@ var leftPressed = false;
 var upPressed = false;
 var downPressed = false;
 var missile = new missile();
-var missiles =[];
-var bossHealth = 50;
-var playerHealth = 10;
+var missiles = [];
+var bossHealth = 30;
+var playerHealth = 100;
+// HealthBar Function & variables-------
+var hBar = $('.health-bar');
+var totalHealth = hBar.data('total');
+var currentHealth = hBar.data('value');
+var bar = hBar.find('.bar');
+
+function healthUpdate(damage) {
+  var newHealth = playerHealth - damage;
+  var barWidth = (newHealth / 100) * 100;
+  bar.css('width', barWidth + "%");
+};
+//------------------------------------------
 var enemies = [{
     left: 100,
     top: 100
@@ -41,6 +53,71 @@ var enemies = [{
   },
   {
     left: 100,
+    top: 250
+  },
+  {
+    left: 200,
+    top: 250
+  },
+  {
+    left: 300,
+    top: 250
+  },
+  {
+    left: 400,
+    top: 250
+  },
+  {
+    left: 500,
+    top: 250
+  },
+  {
+    left: 600,
+    top: 250
+  },
+  {
+    left: 700,
+    top: 250
+  },
+  {
+    left: 800,
+    top: 250
+  }
+];
+var bossEnemies = [{
+    left: 100,
+    top: 100
+  },
+  {
+    left: 200,
+    top: 100
+  },
+  {
+    left: 300,
+    top: 100
+  },
+  {
+    left: 400,
+    top: 100
+  },
+  {
+    left: 500,
+    top: 100
+  },
+  {
+    left: 600,
+    top: 100
+  },
+  {
+    left: 700,
+    top: 100
+  },
+  {
+    left: 800,
+    top: 100
+  },
+  {
+    left: 100,
     top: 175
   },
   {
@@ -70,12 +147,45 @@ var enemies = [{
   {
     left: 800,
     top: 175
+  },
+  {
+    left: 100,
+    top: 250
+  },
+  {
+    left: 200,
+    top: 250
+  },
+  {
+    left: 300,
+    top: 250
+  },
+  {
+    left: 400,
+    top: 250
+  },
+  {
+    left: 500,
+    top: 250
+  },
+  {
+    left: 600,
+    top: 250
+  },
+  {
+    left: 700,
+    top: 250
+  },
+  {
+    left: 800,
+    top: 250
   }
 ];
 
 function random(size) {
   return Math.random() * size + 1
 };
+
 
 var fighter = new Object();
 fighter.element = 'fighter';
@@ -89,7 +199,7 @@ boss.element = 'boss';
 boss.x = 400;
 boss.y = 100;
 boss.w = 192;
-boss.h = 300;
+boss.h = 180;
 
 function setPosition(sprite) {
   var e = document.getElementById(sprite.element);
@@ -98,12 +208,9 @@ function setPosition(sprite) {
   e.style.width = sprite.w + 'px';
   e.style.height = sprite.h + 'px';
 };
-function bossMovement(sprite) {
-  boss.y += random(2)-2;
-  boss.x += random(2)-2;
-  bounds(boss);
-};
-// Movement Loops
+
+
+// // Movement Loops---------------
 $(document).keydown(function(x) {
 
   if (x.keyCode === 37) {
@@ -123,14 +230,13 @@ $(document).keydown(function(x) {
     //Moving Down
     downPressed = true
   }
-  if (x.keyCode === 32 && missiles.length ==0) {
+  if (x.keyCode === 32 && missiles.length == 0) {
     //Space for Fire
     missile.posX = fighter.x;
     missile.posY = fighter.y;
     missile.spawnMissile();
   }
 });
-
 
 $(document).keyup(function(x) {
 
@@ -184,10 +290,9 @@ function bounds(sprite) {
     sprite.y = 700 - sprite.h;
   }
 };
-
-
+//------------------------------
 function drawEnemies() {
-  document.getElementById('enemies').innerHTML = ""
+  document.getElementById('enemies').innerHTML = "";
   for (var i = 0; i < enemies.length; i++) {
     document.getElementById('enemies').innerHTML += `<div class='enemy' style='left:${enemies[i].left}px; top:${enemies[i].top}px'></div>`;
   }
@@ -199,12 +304,51 @@ function moveEnemies() {
       enemies[i].top = enemies[i].top + 0.5;
     } else if (enemies[i].top >= 650) {
       enemies.splice(i, 1);
-      playerHealth -= enemies.length
-      console.log(playerHealth);
+      healthUpdate(20);
+      playerHealth -= 20;
     }
   };
 };
 
+function drawbossEnemies() {
+  document.getElementById('enemies').innerHTML = "";
+  for (var i = 0; i < bossEnemies.length; i++) {
+    document.getElementById('enemies').innerHTML += `<div class='bossEnemy' style='left:${bossEnemies[i].left}px; top:${bossEnemies[i].top}px'></div>`;
+  };
+};
+
+function movebossEnemies() {
+  for (var i = 0; i < bossEnemies.length; i++) {
+    if (bossEnemies[i].top < 650) {
+      bossEnemies[i].top = bossEnemies[i].top + 0.5;
+    } else if (bossEnemies[i].top >= 650) {
+      bossEnemies.splice(i, 1);
+      healthUpdate(10);
+      playerHealth -= 10;
+    }
+  };
+};
+
+//Game Music function-------------//
+function loseCondition() {
+  if (playerHealth <= 0) {
+    gameScreen.style.display = "none";
+    loseScreen.style.display = "block";
+    winScreen.style.display = "none";
+    gameOver.play();
+  }
+};
+
+function winCondition() {
+  if (bossHealth <= 0) {
+    gameScreen.style.display = "none";
+    winScreen.style.display = "block";
+    loseScreen.style.display = "none";
+    bossMusic.stop();
+    gameOver.stop();
+  }
+};
+//-------------------------------//
 function missile() {
   this.velocity = -12;
   this.missileArr = [];
@@ -230,24 +374,33 @@ function missile() {
     };
   };
 };
+
 function bossSpawn() {
-  if (enemies.length == 0) {
-  setPosition(boss);
-  bossMovement();
+  if (enemies.length <= 0 && playerHealth >0) {
+    setPosition(boss);
+    drawbossEnemies();
+    movebossEnemies();
+    checkCollisionBoss();
+    checkCollisionbossEnemies();
+    bossMusic.play();
+    mainSound.stop();
+    if (bossHealth <= 0) {
+      bossMusic.stop();
+    }
   }
 };
+
 
 
 function checkCollision() {
   for (var enemy = 0; enemy < enemies.length; enemy++) {
     for (var missile = 0; missile < missiles.length; missile++) {
       if (
-        missiles[missile].style.left  >= enemies[enemy].left +'px'  &&
-        missiles[missile].style.left <= enemies[enemy].left + 50 +'px' &&
-        missiles[missile].style.top <= enemies[enemy].top +50+'px'  &&
-        missiles[missile].style.top >= enemies[enemy].top+'px'
+        missiles[missile].style.left >= enemies[enemy].left + 'px' &&
+        missiles[missile].style.left <= enemies[enemy].left + 50 + 'px' &&
+        missiles[missile].style.top <= enemies[enemy].top + 50 + 'px' &&
+        missiles[missile].style.top >= enemies[enemy].top + 'px'
       ) {
-        // console.log("hit");
         enemies.splice(enemy, 1);
         missiles.splice(missile, 1);
         $('.missile').remove();
@@ -256,23 +409,60 @@ function checkCollision() {
   };
 };
 
-function checkCollisionBoss(){
-if (enemies.length == 0) {
-  for (var missile = 0; missile < missiles.length; missile++) {
-    if (
-    missiles[missile].style.left  >= boss.x +'px'  &&
-    missiles[missile].style.left <= boss.x + boss.w +'px' &&
-    missiles[missile].style.top <= boss.y + boss.w +'px'  &&
-    missiles[missile].style.top >= boss.y +'px'
-    ) {
-    console.log("hit");
-    $('.missile').remove();
-    bossHealth -= 1/18;
-    console.log(bossHealth);
+function checkCollisionbossEnemies() {
+  for (var enemy = 0; enemy < bossEnemies.length; enemy++) {
+    for (var missile = 0; missile < missiles.length; missile++) {
+      if (
+        missiles[missile].style.left >= bossEnemies[enemy].left + 'px' &&
+        missiles[missile].style.left <= bossEnemies[enemy].left + 50 + 'px' &&
+        missiles[missile].style.top <= bossEnemies[enemy].top + 50 + 'px' &&
+        missiles[missile].style.top >= bossEnemies[enemy].top + 'px'
+      ) {
+        console.log("hit");
+        bossEnemies.splice(enemy, 1);
+        missiles.splice(missile, 1);
+        $('.missile').remove();
+      }
+    }
   };
 };
-}}
 
+function checkCollisionBoss() {
+  for (var missile = 0; missile < missiles.length; missile++) {
+    if (
+      missiles[missile].style.left >= boss.x + 'px' &&
+      missiles[missile].style.left <= boss.x + boss.w + 'px' &&
+      missiles[missile].style.top <= boss.y + boss.h + 'px' &&
+      missiles[missile].style.top >= boss.y + 'px'
+    ) {
+      console.log("hit");
+      $('.missile').remove();
+      bossHealth -= 1 / 2;
+      console.log(bossHealth);
+    };
+  };
+};
+// Game Screens Function-------------//
+var loseScreen = document.getElementById('loseScreen');
+var gameScreen = document.getElementById('background');
+var startScreen = document.getElementById('startScreen');
+var winScreen = document.getElementById('winScreen');
+
+loseScreen.style.display = "none";
+gameScreen.style.display = "none";
+startScreen.style.display = "block";
+winScreen.style.display = "none";
+
+function startGame() {
+  $("#startScreen").hide();
+  $("#background").show();
+  mainSound.play();
+  setInterval(function() {
+    gameLoop();
+  }, 1000 / 60);
+};
+//-----------------------------------//
+// Game Loop-------------//
 function gameLoop() {
   move();
   setPosition(fighter);
@@ -281,28 +471,32 @@ function gameLoop() {
   missile.position();
   missile.fire();
   checkCollision();
-  bossSpawn();
-  checkCollisionBoss();
-
+  setTimeout(function() {
+    bossSpawn();
+  }, 1000);
+  loseCondition();
+  winCondition();
 };
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-        this.sound.play();
-    }
-    this.stop = function(){
-        this.sound.pause();
-    }
-};
-var myMusic;
-myMusic = new sound("./sound/background.mp3");
-myMusic.play()
+//-----------------------------------//
 
-setInterval(function() {
-  gameLoop();
-}, 1000 / 60);x
+//Game Music function-------------//
+var bossMusic = new sound("./sound/bossMusic.mp3", 0.1)
+var mainSound = new sound("./sound/background.mp3", 1);
+var gameOver = new sound("./sound/gameover2.mp3", 0.1);
+
+
+function sound(src, volume, value) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.volume = volume;
+  this.sound.loop = value;
+  this.sound.setAttribute("preload", "auto");
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause();
+  }
+};
+//-----------------------------------//
